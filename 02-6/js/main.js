14d7b180f32aa3535fa47c74887ba9fc1ae7490c
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-01-14 18:14:09 +0200

    Užbaigtas 6 pratimas

--- 02-6/js/main.js
+++ 02-6/js/main.js
@@ -1,12 +1,21 @@
 (() => {
     const buttons = document.querySelectorAll('button');
     const img = document.querySelector('img');
-    const rxTextClasses = /\btext-.+?\b/g;
-    buttons.forEach(button => button.addEventListener('click', (event) => {
+    const cardBody = img.parentElement;
+    const cardBodyClasses = cardBody.className;
+    const flexClassNames = "d-flex flex-column";
+
+    btnClickHandler = (event) => {
         img.className = "";
+        cardBody.className = cardBodyClasses;
         const dataClass = event.target.getAttribute('data-class');
         if (dataClass !== "") {
             img.classList.add(dataClass);
         }
-    }));
+        if (dataClass === "order-12") {
+            cardBody.className += ` ${flexClassNames}`;
+        }
+    };
+
+    buttons.forEach(button => button.addEventListener('click', btnClickHandler));
 })();
